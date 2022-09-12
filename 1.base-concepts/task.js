aeb970ace9d080243f944e876391c7c14467c557
monkidno <92875074+monkidno@users.noreply.github.com>
2022-09-12 10:19:44 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,37 @@
+// task1
+`use strict`
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  let d = b ** 2 - 4 * a * c;
+  console.log("дискриминант: " + d);
 
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr = [x1, x2];
+    console.log(arr);
+    return arr;
+
+  } else if (d === 0) {
+    let x = -b / (2 * a);
+    console.log(x);
+    arr = [x];
+    console.log(arr);
+    return arr;
+    console.log(arr);
+
+
+  } else if (d < 0) {
+    console.log('корней нет');
+    return arr = [];
+
+  }
+}
+solveEquation(-4, 28, -49);
+  return arr; // array
+
+
+  // task2
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
